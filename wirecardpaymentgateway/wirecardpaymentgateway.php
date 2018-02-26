05063f63f5b64a08c24cfc09cdfa513e020a228e
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2018-02-26 13:17:06 +0100

    #1 Add basic info template and basic configuration setup

--- wirecardpaymentgateway/wirecardpaymentgateway.php
+++ wirecardpaymentgateway/wirecardpaymentgateway.php
@@ -32,9 +32,8 @@
 use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
 
 /**
- * Class WirecardEEPaymentGateway
+ * Class WirecardPaymentGateway
  */
-
 class WirecardPaymentGateway extends PaymentModule
 {
     public function __construct()
@@ -49,8 +48,8 @@ class WirecardPaymentGateway extends PaymentModule
 
         parent::__construct();
 
-        $this->displayName = $this->l('Wirecard payment proccesing gateway');
-        $this->description = $this->l('Wirecard payment methods.');
+        $this->displayName = $this->l('Wirecard Payment Processing Gateway');
+        $this->description = $this->l('Wirecard Payment Processing Gateway Plugin for Prestashop.');
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
     }
 
@@ -68,5 +67,90 @@ class WirecardPaymentGateway extends PaymentModule
             return false;
         }
         return true;
+    }
+
+    public function getContent()
+    {
+        if (Tools::isSubmit('btnSubmit')) {
+            $this->_postValidation();
+            if (!count($this->_postErrors)) {
+                $this->_postProcess();
+            } else {
+                foreach ($this->_postErrors as $err) {
+                    $this->_html .= $this->displayError($err);
+                }
+            }
+        } else {
+            $this->_html .= '<br />';
+        }
+
+        $this->_html .= $this->_displayWirecardPaymentGateway();
+        //$this->_html .= $this->renderForm();
+
+        return $this->_html;
+    }
+
+    protected function _displayWirecardPaymentGateway()
+    {
+        return $this->display(__FILE__, 'infos.tpl');
+    }
+
+    //TODO: Form fields for configuration...
+    public function renderForm()
+    {
+        $fields_form = array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->trans('Details', array(), 'Modules.Wirecardpaymentgateway.Admin'),
+                    'icon' => 'icon-envelope'
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'text',
+                        'label' => $this->trans('Wirecard Payment Gateway Title', array(), 'Modules.Wirecardpaymentgateway.Admin'),
+                        'name' => 'WIRECARD_PAYMENT_GATEWAY_TITLE',
+                        'required' => true
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->trans('Save', array(), 'Admin.Actions'),
+                )
+            )
+        );
+
+        /** @var HelperFormCore $helper */
+        $helper = new HelperForm();
+        $helper->show_toolbar = false;
+
+        /** @var LanguageCore $lang */
+        $lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
+        $helper->default_form_language = $lang->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get(
+            'PS_BO_ALLOW_EMPLOYEE_FORM_LANG'
+        ) : 0;
+        $helper->id = (int)Tools::getValue('id_carrier');
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'btnSubmit';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFieldsValues(),
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+            'ajax_configtest_url' => $this->context->link->getAdminLink('AdminModules') . '&configure=' . $this->name
+                . '&tab_module=' . $this->tab . '&module_name=' . $this->name
+        );
+
+        return $helper->generateForm(array($fields_form));
+    }
+
+    //TODO
+    public function getConfigFieldsValues()
+    {
+        return array(
+            'WIRECARD_PAYMENT_GATEWAY_TITLE' => Tools::getValue('WIRECARD_PAYMENT_GATEWAY_TITLE', Configuration::get('WIRECARD_PAYMENT_GATEWAY_TITLE'))
+        );
     }
 }
