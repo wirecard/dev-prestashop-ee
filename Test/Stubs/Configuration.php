2c9c2a26c3f778c7e1d3ab1364aad409f7108347
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-03-14 10:03:55 +0100

    Revert "#7 Delete duplicated folders"
    This reverts commit 963cb632971959752a1dd41e81beca9c78a63623.

---
+++ Test/Stubs/Configuration.php
@@ -0,0 +1,81 @@
+<?php
+
+class Configuration
+{
+    private static $basket;
+    private static $additional;
+
+    public static function setBasketConfig($bool)
+    {
+        self::$basket = $bool;
+    }
+
+    public static function setAdditionalConfig($bool)
+    {
+        self::$additional = $bool;
+    }
+
+    public static function updateValue($param, $value)
+    {
+        return true;
+    }
+
+    public static function get($param)
+    {
+
+        if ('WIRECARD_PAYMENT_GATEWAY_PAYPAL_PAYMENT_ACTION' == $param)
+        {
+            return 'reserve';
+        }
+
+        if ('WIRECARD_PAYMENT_GATEWAY_PAYPAL_BASE_URL' == $param)
+        {
+            return 'https://api-test.wirecard.com';
+        }
+
+        if ('WIRECARD_PAYMENT_GATEWAY_PAYPAL_HTTP_USER' == $param)
+        {
+            return '70000-APITEST-AP';
+        }
+
+        if ('WIRECARD_PAYMENT_GATEWAY_PAYPAL_HTTP_PASS' == $param)
+        {
+            return 'qD2wzQ_hrc!8';
+        }
+
+        if ('WIRECARD_PAYMENT_GATEWAY_PAYPAL_MERCHANT_ACCOUNT_ID' == $param)
+        {
+            return '2a0e9351-24ed-4110-9a1b-fd0fee6bec26';
+        }
+
+        if ('WIRECARD_PAYMENT_GATEWAY_PAYPAL_SECRET' == $param)
+        {
+            return 'dbc5a498-9a66-43b9-bf1d-a618dd399684';
+        }
+
+        if ('WIRECARD_PAYMENT_GATEWAY_PAYPAL_SEND_ADDITIONAL' == $param)
+        {
+            if (self::$additional) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        if ('WIRECARD_PAYMENT_GATEWAY_PAYPAL_SHOPPING_BASKET' == $param)
+        {
+            if (self::$basket) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        if ('WIRECARD_PAYMENT_GATEWAY_PAYPAL_DESCRIPTOR' == $param)
+        {
+            return 1;
+        }
+
+        return $param;
+    }
+}
