3cb07486c30375807c79add6d5a00e86f880a9b3
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2018-03-15 13:34:05 +0100

    #13 Add objectmodel stub

--- Test/Stubs/ObjectModel.php
+++ Test/Stubs/ObjectModel.php
@@ -33,36 +33,6 @@
  * @license GPLv3
  */
 
-const _PS_MODULE_DIR_ = './';
-const _DB_PREFIX_ = 'Prefix_';
-const _MYSQL_ENGINE_ = 'mysql';
-
-require_once __DIR__ . '/../wirecardpaymentgateway/vendor/autoload.php';
-
-//stub objects
-require __DIR__ . '/Stubs/Currency.php';
-require __DIR__ . '/Stubs/ObjectModel.php';
-require __DIR__ . '/Stubs/Controller.php';
-require __DIR__ . '/Stubs/ModuleFrontController.php';
-require __DIR__ . '/Stubs/ModuleAdminController.php';
-require __DIR__ . '/Stubs/Module.php';
-require __DIR__ . '/Stubs/PaymentModule.php';
-require __DIR__ . '/Stubs/Tools.php';
-require __DIR__ . '/Stubs/Configuration.php';
-require __DIR__ . '/Stubs/HelperForm.php';
-require __DIR__ . '/Stubs/Language.php';
-require __DIR__ . '/Stubs/Context.php';
-require __DIR__ . '/Stubs/Link.php';
-require __DIR__ . '/Stubs/Smarty.php';
-require __DIR__ . '/Stubs/Media.php';
-require __DIR__ . '/Stubs/PaymentOption.php';
-require __DIR__ . '/Stubs/Cart.php';
-require __DIR__ . '/Stubs/Customer.php';
-require __DIR__ . '/Stubs/Address.php';
-require __DIR__ . '/Stubs/Country.php';
-require __DIR__ . '/Stubs/PrestaShopLogger.php';
-require __DIR__ . '/Stubs/Db.php';
-require __DIR__ . '/Stubs/Tab.php';
-
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'de';
+class ObjectModel
+{
+}
