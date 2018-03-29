85dc8a411473a1ab40fd25565a8d5ad2953e4ab6
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2018-03-29 09:31:39 +0200

    #17 Sort methods, update headers

--- wirecardpaymentgateway/wirecardpaymentgateway.php
+++ wirecardpaymentgateway/wirecardpaymentgateway.php
@@ -58,6 +58,10 @@ class WirecardPaymentGateway extends PaymentModule
      */
     private $config;
 
+    /**
+     * @var string
+     * @since 1.0.0
+     */
     protected $html;
 
     /**
@@ -142,7 +146,9 @@ class WirecardPaymentGateway extends PaymentModule
     }
 
     /**
-     * register tabs
+     * Register tabs
+     *
+     * @since 1.0.0
      */
     public function installTabs()
     {
@@ -157,6 +163,11 @@ class WirecardPaymentGateway extends PaymentModule
         $tab->add();
     }
 
+    /**
+     * Unregister tabs
+     *
+     * @since 1.0.0
+     */
     public function uninstallTabs()
     {
         $id_tab = (int)Tab::getIdFromClassName('WirecardTransactions');
@@ -261,45 +272,6 @@ class WirecardPaymentGateway extends PaymentModule
         }
 
         return $params;
-    }
-
-    /**
-     * return available country iso codes
-     *
-     * @return array
-     * @since 1.0.0
-     */
-    protected function getCountries()
-    {
-        $cookie = $this->context->cookie;
-        $countries = Country::getCountries($cookie->id_lang);
-        $ret = array();
-        foreach ($countries as $country) {
-            $ret[] = array(
-                'key' => $country['iso_code'],
-                'value' => $country['name']
-            );
-        }
-        return $ret;
-    }
-
-    /**
-     * return available currency iso codes
-     *
-     * @return array
-     * @since 1.0.0
-     */
-    protected function getCurrencies()
-    {
-        $currencies = Currency::getCurrencies();
-        $ret = array();
-        foreach ($currencies as $currency) {
-            $ret[] = array(
-                'key' => $currency['iso_code'],
-                'value' => $currency['name']
-            );
-        }
-        return $ret;
     }
 
     /**
@@ -444,6 +416,95 @@ class WirecardPaymentGateway extends PaymentModule
     }
 
     /**
+     * Hook for media setter
+     *
+     * @return bool
+     * @since 1.0.0
+     */
+    public function hookActionFrontControllerSetMedia()
+    {
+        $link = new Link;
+        $parameters = array("action" => "getcreditcardconfig");
+        $ajaxLink = $link->getModuleLink('wirecardpaymentgateway', 'creditcard', $parameters);
+        $baseUrl = $this->getConfigValue('creditcard', 'base_url');
+        Media::addJsDef(array('url' => $ajaxLink));
+        $this->context->controller->addJquery();
+        $this->context->controller->addJqueryUI('dialog');
+        $this->context->controller->registerJavascript(
+            'remote-bootstrap',
+            $baseUrl  .'/engine/hpp/paymentPageLoader.js',
+            array('server' => 'remote', 'position' => 'head', 'priority' => 20)
+        );
+
+        foreach ($this->getPayments() as $paymentMethod) {
+            if ($paymentMethod->getLoadJs()) {
+                $ajaxLink = $link->getModuleLink('wirecardpaymentgateway', $paymentMethod->getType());
+                Media::addJsDef(array('ajax'.$paymentMethod->getType().'url' => $ajaxLink));
+                $this->context->controller->addJS(
+                    _PS_MODULE_DIR_ . $this->name . DIRECTORY_SEPARATOR . 'views'
+                    . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . $paymentMethod->getType() . '.js'
+                );
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Create redirect Urls
+     *
+     * @param $paymentState
+     * @return null
+     * @since 1.0.0
+     */
+    public function createRedirectUrl($cartId, $paymentType, $paymentState)
+    {
+        $returnUrl = $this->context->link->getModuleLink(
+            $this->name,
+            'return',
+            array(
+                'id_cart' => $cartId,
+                'payment_type' => $paymentType,
+                'payment_state' => $paymentState,
+            )
+        );
+
+        return $returnUrl;
+    }
+
+    /**
+     * Create notification Urls
+     *
+     * @return null
+     * @since 1.0.0
+     */
+    public function createNotificationUrl($cartId, $paymentType)
+    {
+        $returnUrl = $this->context->link->getModuleLink(
+            $this->name,
+            'notify',
+            array(
+                'id_cart' => $cartId,
+                'payment_type' => $paymentType,
+            )
+        );
+
+        return $returnUrl;
+    }
+
+    /**
+     * Set the name to the payment type selected
+     *
+     * @param $params
+     * @since 1.0.0
+     */
+    public function hookActionPaymentConfirmation($params)
+    {
+        $order = new Order($params['id_order']);
+        $this->displayName = $order->payment;
+    }
+
+    /**
      * Display info text for Wirecard Payment Processing Gateway page
      *
      * @return string
@@ -452,6 +513,45 @@ class WirecardPaymentGateway extends PaymentModule
     protected function displayWirecardPaymentGateway()
     {
         return $this->display(__FILE__, 'infos.tpl');
+    }
+
+    /**
+     * return available country iso codes
+     *
+     * @return array
+     * @since 1.0.0
+     */
+    protected function getCountries()
+    {
+        $cookie = $this->context->cookie;
+        $countries = Country::getCountries($cookie->id_lang);
+        $ret = array();
+        foreach ($countries as $country) {
+            $ret[] = array(
+                'key' => $country['iso_code'],
+                'value' => $country['name']
+            );
+        }
+        return $ret;
+    }
+
+    /**
+     * return available currency iso codes
+     *
+     * @return array
+     * @since 1.0.0
+     */
+    protected function getCurrencies()
+    {
+        $currencies = Currency::getCurrencies();
+        $ret = array();
+        foreach ($currencies as $currency) {
+            $ret[] = array(
+                'key' => $currency['iso_code'],
+                'value' => $currency['name']
+            );
+        }
+        return $ret;
     }
 
     /**
@@ -695,7 +795,12 @@ class WirecardPaymentGateway extends PaymentModule
         return true;
     }
 
-
+    /**
+     * Create wirecard payment transaction table
+     *
+     * @return bool
+     * @since 1.0.0
+     */
     private function createTable()
     {
         $sql = 'CREATE TABLE IF NOT EXISTS  `' . _DB_PREFIX_ . 'wirecard_payment_gateway_tx` (';
@@ -712,6 +817,12 @@ class WirecardPaymentGateway extends PaymentModule
         return Db::getInstance()->execute($sql);
     }
 
+    /**
+     * Get transaction table columns
+     *
+     * @return array
+     * @since 1.0.0
+     */
     private function getColumnDefs()
     {
         return array(
@@ -730,88 +841,5 @@ class WirecardPaymentGateway extends PaymentModule
             "created" => array("DATETIME", "NOT NULL"),
             "modified" => array("DATETIME", "NULL"),
         );
-    }
-
-    public function hookActionFrontControllerSetMedia()
-    {
-        $link = new Link;
-        $parameters = array("action" => "getcreditcardconfig");
-        $ajaxLink = $link->getModuleLink('wirecardpaymentgateway', 'creditcard', $parameters);
-        $baseUrl = $this->getConfigValue('creditcard', 'base_url');
-        Media::addJsDef(array('url' => $ajaxLink));
-        $this->context->controller->addJquery();
-        $this->context->controller->addJqueryUI('dialog');
-        $this->context->controller->registerJavascript(
-            'remote-bootstrap',
-            $baseUrl  .'/engine/hpp/paymentPageLoader.js',
-            array('server' => 'remote', 'position' => 'head', 'priority' => 20)
-        );
-
-        foreach ($this->getPayments() as $paymentMethod) {
-            if ($paymentMethod->getLoadJs()) {
-                $ajaxLink = $link->getModuleLink('wirecardpaymentgateway', $paymentMethod->getType());
-                Media::addJsDef(array('ajax'.$paymentMethod->getType().'url' => $ajaxLink));
-                $this->context->controller->addJS(
-                    _PS_MODULE_DIR_ . $this->name . DIRECTORY_SEPARATOR . 'views'
-                    . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . $paymentMethod->getType() . '.js'
-                );
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Create redirect Urls
-     *
-     * @param $paymentState
-     * @return null
-     * @since 1.0.0
-     */
-    public function createRedirectUrl($cartId, $paymentType, $paymentState)
-    {
-        $returnUrl = $this->context->link->getModuleLink(
-            $this->name,
-            'return',
-            array(
-                'id_cart' => $cartId,
-                'payment_type' => $paymentType,
-                'payment_state' => $paymentState,
-            )
-        );
-
-        return $returnUrl;
-    }
-
-    /**
-     * Create notification Urls
-     *
-     * @return null
-     * @since 1.0.0
-     */
-    public function createNotificationUrl($cartId, $paymentType)
-    {
-        $returnUrl = $this->context->link->getModuleLink(
-            $this->name,
-            'notify',
-            array(
-                'id_cart' => $cartId,
-                'payment_type' => $paymentType,
-            )
-        );
-
-        return $returnUrl;
-    }
-
-    /**
-     * Set the name to the payment type selected
-     *
-     * @param $params
-     * @since 1.0.0
-     */
-    public function hookActionPaymentConfirmation($params)
-    {
-        $order = new Order($params['id_order']);
-        $this->displayName = $order->payment;
     }
 }
