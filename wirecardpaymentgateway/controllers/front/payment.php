fd4e92ca9e1630ce323593465f18f242b60e29f7
jakubpolomsky <jakub.polomsky@partner.wirecard.com>
2018-05-30 09:44:16 +0200

    use smarty template instead of html generated in php

--- wirecardpaymentgateway/controllers/front/payment.php
+++ wirecardpaymentgateway/controllers/front/payment.php
@@ -153,9 +153,7 @@ class WirecardPaymentGatewayPaymentModuleFrontController extends ModuleFrontCont
             $data['url']         = $response->getUrl();
             $data['method']      = $response->getMethod();
             $data['form_fields'] = $response->getFormFields();
-
-            echo $this->createPostForm($data);
-            die();
+            die($this->createPostForm($data));
         } elseif ($response instanceof FailureResponse) {
             $errors = '';
             foreach ($response->getStatusCollection()->getIterator() as $item) {
@@ -177,26 +175,19 @@ class WirecardPaymentGatewayPaymentModuleFrontController extends ModuleFrontCont
      */
     private function createPostForm($data)
     {
-        $html  = '';
-        $html .= '<script>window.setInterval( function() {
-                    var wait = document.getElementById( "wait" );
-    				if ( wait.innerHTML.length > 3 ) 
-       					 wait.innerHTML = "";
-    				else 
-        				wait.innerHTML += ".";
-    		}, 200); 
-    		</script>
-			<div style="display: flex; justify-content: center; font-size: 20px;">' .
-            'You are being redirected. Please wait' . '
-			<span id="wait" style="font-size: 20px; width: 50px;">.</span></div>';
-        $html .= '<form id="credit_card_form" method="' . $data['method'] . '" action="' . $data['url'] . '">';
-        foreach ($data['form_fields'] as $key => $value) {
-            $html .= '<input type="hidden" name="' . $key . '" value="' . $value . '">';
-        }
-        $html .= '</form>';
-        $html .= '<script>document.getElementsByTagName("form")[0].submit();</script>';
+        $logger = new \WirecardEE\Prestashop\Helper\Logger();
+        try {
+            $this->context->smarty->assign($data);
 
-        return $html;
+            return $this->context->smarty->fetch(_PS_MODULE_DIR_ . 'wirecardpaymentgateway' . DIRECTORY_SEPARATOR .
+                'views' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . 'front' . DIRECTORY_SEPARATOR .
+                'creditcard_submitform.tpl');
+        } catch (SmartyException $e) {
+            $logger->error($e->getMessage());
+        } catch (Exception $e) {
+            $logger->error($e->getMessage());
+        }
+        return '';
     }
 
     /**
