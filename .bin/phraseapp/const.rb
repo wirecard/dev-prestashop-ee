dbe29582520eaad9ea41fdfec106d35a458baa8f
Nikolaos Stamatopoulos <stamatopoulos@parkside.at>
2019-01-22 10:46:46 +0100

    Use frozen_string_literal in Ruby

--- .bin/phraseapp/const.rb
+++ .bin/phraseapp/const.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 module Const
-  GITHUB_PHRASEAPP_PR_TITLE = '[PhraseApp] Update locales'.freeze
-  GITHUB_PHRASEAPP_PR_BODY = 'Update locales from PhraseApp'.freeze
-  GIT_PHRASEAPP_COMMIT_MSG = '[skip ci] Update translations from PhraseApp'.freeze
-  GIT_PHRASEAPP_BRANCH_BASE = 'master'.freeze
-  PHRASEAPP_PROJECT_ID = '9036e89959d471e0c2543431713b7ba1'.freeze
-  PHRASEAPP_FALLBACK_LOCALE = 'en_US'.freeze
+  GITHUB_PHRASEAPP_PR_TITLE = '[PhraseApp] Update locales'
+  GITHUB_PHRASEAPP_PR_BODY = 'Update locales from PhraseApp'
+  GIT_PHRASEAPP_COMMIT_MSG = '[skip ci] Update translations from PhraseApp'
+  GIT_PHRASEAPP_BRANCH_BASE = 'master'
+  PHRASEAPP_PROJECT_ID = '9036e89959d471e0c2543431713b7ba1'
+  PHRASEAPP_FALLBACK_LOCALE = 'en_US'
 
   # project-specific mappings for locales to filenames
-  PHRASEAPP_TAG = 'prestashop'.freeze
+  PHRASEAPP_TAG = 'prestashop'
   LOCALE_SPECIFIC_MAP = {
     'en_US': 'en',
     'de_DE': 'de',
@@ -16,10 +18,10 @@ module Const
     'ko_KR': 'ko',
     'pl_PL': 'pl',
     'zh_TW': 'tw',
-    'zh_CN': 'zh'
+    'zh_CN': 'zh',
   }.freeze
 
   # paths relative to project root
-  PLUGIN_DIR = 'wirecardpaymentgateway'.freeze
-  PLUGIN_I18N_DIR = File.join(PLUGIN_DIR, 'translations').freeze
+  PLUGIN_DIR = 'wirecardpaymentgateway'
+  PLUGIN_I18N_DIR = File.join(PLUGIN_DIR, 'translations')
 end
