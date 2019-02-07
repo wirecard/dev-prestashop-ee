e0949ea5afaee225890e7f44c0bce801497b6678
Sebastian Russmann <sebastian.russmann@wirecard.com>
2019-02-07 10:17:44 +0100

    Update docker to use env variables

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #set -e # Exit with nonzero exit code if anything fails
 
+#move this to its own script
+#
+
 export PRESTASHOP_CONTAINER_NAME=prestashop-web
 export PRESTASHOP_CONTAINER_DOMAIN=localhost:8080
 export PRESTASHOP_CONTAINER_SHOP_URL=localhost:8080
 
-echo "PRESTASHOP_CONTAINER_NAME=prestashop-web" >> ../.env
-echo "PRESTASHOP_CONTAINER_DOMAIN=localhost:8080" >> ../.env
-echo "PRESTASHOP_CONTAINER_SHOP_URL=localhost:8080" >> ../.env
+#echo "PRESTASHOP_CONTAINER_NAME=prestashop-web" >> ../.env
+#echo "PRESTASHOP_CONTAINER_DOMAIN=localhost:8080" >> ../.env
+#echo "PRESTASHOP_CONTAINER_SHOP_URL=localhost:8080" >> ../.env
 
-sudo docker-compose up --force-recreate
+docker-compose build --no-cache --build-arg PRESTASHOP_CONTAINER_NAME=${PRESTASHOP_CONTAINER_NAME} --build-arg PRESTASHOP_CONTAINER_DOMAIN=${PRESTASHOP_CONTAINER_DOMAIN} --build-arg PRESTASHOP_CONTAINER_SHOP_URL=${PRESTASHOP_CONTAINER_SHOP_URL} prestashop.web
+docker-compose up --force-recreate -d
 
 # wait for the host to startup
 while ! $(curl --output /dev/null --silent --head --fail "http://${PRESTASHOP_CONTAINER_SHOP_URL}/backend/index.php"); do
@@ -18,4 +22,4 @@ while ! $(curl --output /dev/null --silent --head --fail "http://${PRESTASHOP_CO
 done
 
 # install the plugin
-sudo docker exec prestashop-web php /var/www/html/bin/console prestashop:module install wirecardpaymentgateway
+docker exec prestashop-web php /var/www/html/bin/console prestashop:module install wirecardpaymentgateway
