6f6095e85da08d85203b995a53ef175512c3f3dd
Mihael Lovrencic <mihael.lovrencic@wirecard.com>
2019-04-18 12:28:31 +0200

    #122 upgrade script for payment_action

--- wirecardpaymentgateway/upgrade/upgrade-1.4.0.php
+++ wirecardpaymentgateway/upgrade/upgrade-1.4.0.php
@@ -6,6 +6,15 @@ if (!defined('_PS_VERSION_')) {
 
 function upgrade_module_1_4_0($object)
 {
-    return Db::getInstance()->execute('ALTER TABLE `'._DB_PREFIX_.
-        'wirecard_payment_gateway_cc` ADD `last_used` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `masked_pan`');
+    $sql = 'ALTER TABLE `'._DB_PREFIX_.
+        'wirecard_payment_gateway_cc` ADD `last_used` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `masked_pan`;';
+    $sql .='UPDATE `'._DB_PREFIX_.'configuration` SET `value` = "purchase" WHERE'.
+        ' `name` = "WIRECARD_PAYMENT_GATEWAY_CREDITCARD_PAYMENT_ACTION" AND `value`="pay";';
+    $sql .='UPDATE `'._DB_PREFIX_.'configuration` SET `value` = "authorization" WHERE'.
+        ' `name` = "WIRECARD_PAYMENT_GATEWAY_CREDITCARD_PAYMENT_ACTION" AND `value`="reserve";';
+    $sql .='UPDATE `'._DB_PREFIX_.'configuration` SET `value` = "purchase" WHERE'.
+        ' `name` = "WIRECARD_PAYMENT_GATEWAY_UNIONPAYINTERNATIONAL_PAYMENT_ACTION" AND `value`="pay";';
+    $sql .='UPDATE `'._DB_PREFIX_.'configuration` SET `value` = "authorization" WHERE'.
+        ' `name` = "WIRECARD_PAYMENT_GATEWAY_UNIONPAYINTERNATIONAL_PAYMENT_ACTION" AND `value`="reserve";';
+    return Db::getInstance()->execute($sql);
 }
