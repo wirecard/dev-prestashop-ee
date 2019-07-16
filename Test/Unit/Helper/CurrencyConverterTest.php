b633afeba83d9ff2f79827c98c0c093c73a6b58e
Richard Blechinger <me@blechi.at>
2019-07-16 13:43:39 +0200

    Fix: Adapt code style for tests

--- Test/Unit/Helper/CurrencyConverterTest.php
+++ Test/Unit/Helper/CurrencyConverterTest.php
@@ -2,21 +2,25 @@
 
 use WirecardEE\Prestashop\Helper\CurrencyConverter;
 
-class CurrencyConverterTest extends \PHPUnit_Framework_TestCase {
+class CurrencyConverterTest extends \PHPUnit_Framework_TestCase
+{
 
     /** @var CurrencyConverter */
     private $converter;
 
-    public function setUp() {
+    public function setUp()
+    {
         $this->converter = new CurrencyConverter();
     }
 
-    public function testItConvertsTheCurrencyCorrectly() {
+    public function testItConvertsTheCurrencyCorrectly()
+    {
         $convertedAmount = $this->converter->convertToCurrency(50.0, 'USD');
         $this->assertEquals(25.0, $convertedAmount);
     }
 
-    public function testItHandlesMissingCurrenciesGracefully() {
+    public function testItHandlesMissingCurrenciesGracefully()
+    {
         $convertedAmount = $this->converter->convertToCurrency(250.0, 'PHP');
         $this->assertEquals(250.0, $convertedAmount);
     }
