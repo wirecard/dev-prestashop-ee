e35a6d5401ae9a0a5322935dc09fa27d02ebc785
Tatjana Starcenko <tatjana.starcenko@wirecard.com>
2019-10-16 14:08:40 +0200

    Correct plugin name in license

--- wirecardpaymentgateway/tests/_support/ActorExtendedWithWrappers.php
+++ wirecardpaymentgateway/tests/_support/ActorExtendedWithWrappers.php
@@ -2,9 +2,9 @@
 /**
  * Shop System Plugins:
  * - Terms of Use can be found under:
- * https://github.com/wirecard/magento2-ee/blob/master/_TERMS_OF_USE
+ * https://github.com/wirecard/prestashop-ee/blob/master/_TERMS_OF_USE
  * - License can be found under:
- * https://github.com/wirecard/magento2-ee/blob/master/LICENSE
+ * https://github.com/wirecard/prestashop-ee/blob/master/LICENSE
  */
 
 namespace Wirecard\ElasticEngine\tests\_support;
