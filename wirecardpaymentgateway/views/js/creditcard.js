da63f08abd501f4cdbe0a5b68c1097ff99a9e93d
Richard Blechinger <me@blechi.at>
2019-10-29 08:10:30 +0100

    First round of Codacy adjustments

--- wirecardpaymentgateway/views/js/creditcard.js
+++ wirecardpaymentgateway/views/js/creditcard.js
@@ -19,20 +19,20 @@ var Constants = {
     MODAL_ID: "#wirecard-ccvault-modal",
     IFRAME_ID: "#wirecard-integrated-payment-page-frame",
     CONTAINER_ID: "payment-processing-gateway-credit-card-form",
-    PAYMENT_FORM_ID: '#payment-form[action*="creditcard"]',
-    PAYMENT_METHOD_RADIO_ID: 'input[name="payment-option"][data-module-name="wd-creditcard"]',
+    PAYMENT_FORM_ID: "#payment-form[action*=\"creditcard\"]",
+    PAYMENT_METHOD_RADIO_ID: "input[name=\"payment-option\"][data-module-name=\"wd-creditcard\"]",
     USE_CARD_BUTTON_ID: "button[data-tokenid]",
     DELETE_CARD_BUTTON_ID: "button[data-cardid]",
     STORED_CARD_BUTTON_ID: "#stored-card",
     SAVE_CARD_CHECKMARK_ID: "#wirecard-store-card",
     CARD_LIST_ID: "#wd-card-list",
     CARD_SPINNER_ID: "#card-spinner"
-}
+};
 
 var SpinnerState = {
     HIDDEN: "none",
     VISIBLE: "block"
-}
+};
 
 /*
  * DOM manipulation functions
@@ -46,20 +46,20 @@ var SpinnerState = {
  */
 function setSpinnerState(state)
 {
-    var $button = jQuery(Constants.STORED_CARD_BUTTON_ID)
-    var $container = jQuery("#" + Constants.CONTAINER_ID)
+    var $button = jQuery(Constants.STORED_CARD_BUTTON_ID);
+    var $container = jQuery("#" + Constants.CONTAINER_ID);
 
-    jQuery(Constants.CARD_SPINNER_ID).css("display", state)
+    jQuery(Constants.CARD_SPINNER_ID).css("display", state);
 
     if (SpinnerState.HIDDEN === state) {
-        $button.removeAttr("disabled")
-        $container.css("display", "block")
+        $button.removeAttr("disabled");
+        $container.css("display", "block");
 
-        return
+        return;
     }
 
-    $button.attr("disabled", "disabled")
-    $container.css("display", "none")
+    $button.attr("disabled", "disabled");
+    $container.css("display", "none");
 }
 
 /**
@@ -76,9 +76,9 @@ function attachFormField($form, name, value)
         type: "hidden",
         value: value,
         name: name,
-    })
+    });
 
-    $form.append($input)
+    $form.append($input);
 }
 
 /**
@@ -88,14 +88,14 @@ function attachFormField($form, name, value)
  */
 function setIframeSize()
 {
-    var $iframe = jQuery(Constants.IFRAME_ID)
-    var $window = jQuery(window)
+    var $iframe = jQuery(Constants.IFRAME_ID);
+    var $window = jQuery(window);
 
     if ($window.width() < Constants.IFRAME_HEIGHT_CUTOFF) {
-        $iframe.height(Constants.IFRAME_HEIGHT_MOBILE)
+        $iframe.height(Constants.IFRAME_HEIGHT_MOBILE);
     }
 
-    $iframe.height(Constants.IFRAME_HEIGHT_DESKTOP)
+    $iframe.height(Constants.IFRAME_HEIGHT_DESKTOP);
 }
 
 /**
@@ -105,11 +105,168 @@ function setIframeSize()
  */
 function submitFormToShop()
 {
-    var $document = jQuery(document)
-    var $form = jQuery(document.querySelector(Constants.PAYMENT_FORM_ID))
+    var $document = jQuery(document);
+    var $form = jQuery(document.querySelector(Constants.PAYMENT_FORM_ID));
 
-    $document.off("submit", Constants.PAYMENT_FORM_ID)
-    $form.submit()
+    $document.off("submit", Constants.PAYMENT_FORM_ID);
+    $form.submit();
+}
+
+/*
+ * Business logic functions
+ */
+
+/**
+ * Error handling for generic AJAX requests
+ *
+ * @param error
+ * @since 2.4.0
+ */
+function onError(error)
+{
+    console.error("Run-time error:", error.responseText);
+}
+
+/**
+ * Error handling for WPP errors
+ *
+ * @param error
+ * @since 2.4.0
+ */
+function onFormError(error)
+{
+    console.error("Form error:", error);
+
+    initializeForm();
+}
+
+/**
+ * Handles the seamless response and formats it so we can handle it in the
+ * backend of our shop
+ *
+ * @param data
+ * @since 2.4.0
+ */
+function onSeamlessFormSubmit(data)
+{
+    var $form = jQuery(document.querySelector(Constants.PAYMENT_FORM_ID));
+    var $checkmark = jQuery(Constants.SAVE_CARD_CHECKMARK_ID);
+    var shouldSaveCard = $checkmark.prop('checked');
+
+    attachFormField($form, 'jsresponse', 'true');
+
+    for (var prop in data) {
+        if (data.hasOwnProperty(prop)) {
+            attachFormField($form, prop, data[prop]);
+        }
+    }
+
+    if (
+        shouldSaveCard
+        && data.hasOwnProperty("token_id")
+        && data.hasOwnProperty("masked_account_number")
+    ) {
+        saveCardAndSubmitToShop(data["token_id"], data["masked_account_number"]);
+        return;
+    }
+
+    submitFormToShop();
+}
+
+/**
+ * Intercepts the submission of the shop payment form to submit our seamless form
+ *
+ * @param event
+ * @since 2.4.0
+ */
+function onPaymentFormSubmit(event)
+{
+    event.preventDefault();
+
+    WPP.seamlessSubmit({
+        wrappingDivId: Constants.CONTAINER_ID,
+        onSuccess: onSeamlessFormSubmit,
+        onError: onFormError
+    });
+}
+
+/**
+ * Handles the cleanup after the seamless form has been rendered
+ *
+ * @since 2.4.0
+ */
+function onFormRendered()
+{
+    setSpinnerState(SpinnerState.HIDDEN);
+    setIframeSize();
+}
+
+/**
+ * Takes the transaction built in the backend and renders the seamless form
+ *
+ * @param formData
+ * @since 2.4.0
+ */
+function onFormDataReceived(formData)
+{
+    jQuery(Constants.MODAL_ID).modal('hide');
+
+    formData.wpp_options_cvv_hidden = true;
+
+    WPP.seamlessRender({
+        requestData: formData,
+        wrappingDivId: Constants.CONTAINER_ID,
+        onSuccess: onFormRendered,
+        onError: onFormError
+    });
+}
+
+/**
+ * Updates the card list for one-click checkout
+ *
+ * @param cardList
+ * @since 2.4.0
+ */
+function onCardListReceived(cardList)
+{
+    jQuery(Constants.CARD_LIST_ID).html(cardList.html);
+}
+
+/**
+ * Handles the deletion of a card
+ *
+ * @since 2.4.0
+ */
+function onCardDeletion()
+{
+    var $button = jQuery(this);
+    var cardId = $button.data('cardid');
+
+    deleteCard(cardId);
+}
+
+/**
+ * Reloads the seamless form with a pre-existing token
+ *
+ * @since 2.4.0
+ */
+function onCardSelected()
+{
+    var $button = jQuery(this);
+    var tokenId = $button.data('tokenid');
+
+    setSpinnerState(SpinnerState.VISIBLE);
+    initializeForm(tokenId);
+}
+
+/**
+ * Initializes the form once credit card is selected
+ *
+ * @since 2.4.0
+ */
+function onPaymentMethodSelected()
+{
+    initializeForm();
 }
 
 /*
@@ -132,20 +289,22 @@ function getFormData(tokenId = null)
             cartId: cartId,
             tokenId: tokenId
         }
-    })
+    });
 
     formDataRequest
         .done(onFormDataReceived)
-        .fail(onFormError)
+        .fail(onFormError);
 }
 
 /**
  * Gets all available cards for this customer
+ *
+ * @since 2.4.0
  */
 function getCardList()
 {
     if (!ccVaultEnabled) {
-        return
+        return;
     }
 
     var cardListRequest = jQuery.ajax({
@@ -153,11 +312,11 @@ function getCardList()
         data: {
             action: "liststoredcards"
         }
-    })
+    });
 
     cardListRequest
         .done(onCardListReceived)
-        .fail(onError)
+        .fail(onError);
 }
 
 /**
@@ -176,17 +335,18 @@ function saveCardAndSubmitToShop(tokenId, maskedPan)
             tokenId: tokenId,
             maskedPan: maskedPan
         }
-    })
+    });
 
     cardSavingRequest
         .done(submitFormToShop)
-        .fail(onError)
+        .fail(onError);
 }
 
 /**
  * Deletes the saved card from the backend
  *
  * @param cardId
+ * @since 2.4.0
  */
 function deleteCard(cardId)
 {
@@ -196,167 +356,11 @@ function deleteCard(cardId)
             action: "deletecard",
             cardId: cardId
         }
-    })
+    });
 
     cardDeletionRequest
         .done(onCardListReceived)
-        .fail(onError)
-}
-
-/*
- * Business logic functions
- */
-
-/**
- * Error handling for generic AJAX requests
- *
- * @param error
- */
-function onError(error)
-{
-    console.error("Run-time error:", error.responseText)
-}
-
-/**
- * Error handling for WPP errors
- *
- * @param error
- * @since 2.4.0
- */
-function onFormError(error)
-{
-    console.error("Form error:", error)
-
-    initializeForm()
-}
-
-/**
- * Handles the seamless response and formats it so we can handle it in the
- * backend of our shop
- *
- * @param data
- * @since 2.4.0
- */
-function onSeamlessFormSubmit(data)
-{
-    var $form = jQuery(document.querySelector(Constants.PAYMENT_FORM_ID))
-    var $checkmark = jQuery(Constants.SAVE_CARD_CHECKMARK_ID)
-    var shouldSaveCard = $checkmark.prop('checked')
-
-    attachFormField($form, 'jsresponse', 'true')
-
-    for (var prop in data) {
-        if (data.hasOwnProperty(prop)) {
-            attachFormField($form, prop, data[prop])
-        }
-    }
-
-    if (
-        shouldSaveCard
-        && data.hasOwnProperty("token_id")
-        && data.hasOwnProperty("masked_account_number")
-    ) {
-        saveCardAndSubmitToShop(data["token_id"], data["masked_account_number"])
-        return
-    }
-
-    submitFormToShop()
-}
-
-/**
- * Intercepts the submission of the shop payment form to submit our seamless form
- *
- * @param event
- * @since 2.4.0
- */
-function onPaymentFormSubmit(event)
-{
-    event.preventDefault()
-
-    WPP.seamlessSubmit({
-        wrappingDivId: Constants.CONTAINER_ID,
-        onSuccess: onSeamlessFormSubmit,
-        onError: onFormError
-    })
-}
-
-/**
- * Handles the cleanup after the seamless form has been rendered
- *
- * @since 2.4.0
- */
-function onFormRendered()
-{
-    setSpinnerState(SpinnerState.HIDDEN)
-    setIframeSize()
-}
-
-/**
- * Takes the transaction built in the backend and renders the seamless form
- *
- * @param formData
- * @since 2.4.0
- */
-function onFormDataReceived(formData)
-{
-    jQuery(Constants.MODAL_ID).modal('hide')
-
-    formData.wpp_options_cvv_hidden = true
-
-    WPP.seamlessRender({
-        requestData: formData,
-        wrappingDivId: Constants.CONTAINER_ID,
-        onSuccess: onFormRendered,
-        onError: onFormError
-    })
-}
-
-/**
- * Updates the card list for one-click checkout
- *
- * @param cardList
- * @since 2.4.0
- */
-function onCardListReceived(cardList)
-{
-    jQuery(Constants.CARD_LIST_ID).html(cardList.html)
-}
-
-/**
- * Handles the deletion of a card
- *
- * @since 2.4.0
- */
-function onCardDeletion()
-{
-    var $button = jQuery(this)
-    var cardId = $button.data('cardid')
-
-    deleteCard(cardId)
-}
-
-/**
- * Reloads the seamless form with a pre-existing token
- *
- * @since 2.4.0
- */
-function onCardSelected()
-{
-    var $button = jQuery(this)
-    var tokenId = $button.data('tokenid')
-
-    setSpinnerState(SpinnerState.VISIBLE)
-    initializeForm(tokenId)
-}
-
-/**
- * Initializes the form once credit card is selected
- *
- * @since 2.4.0
- */
-function onPaymentMethodSelected()
-{
-    initializeForm()
+        .fail(onError);
 }
 
 /*
@@ -370,12 +374,12 @@ function onPaymentMethodSelected()
  */
 function initializeHandlers()
 {
-    var $document = jQuery(document)
+    var $document = jQuery(document);
 
-    $document.on("click", Constants.PAYMENT_METHOD_RADIO_ID, onPaymentMethodSelected)
-    $document.on("click", Constants.DELETE_CARD_BUTTON_ID, onCardDeletion)
-    $document.on("click", Constants.USE_CARD_BUTTON_ID, onCardSelected)
-    $document.on("submit", Constants.PAYMENT_FORM_ID, onPaymentFormSubmit)
+    $document.on("click", Constants.PAYMENT_METHOD_RADIO_ID, onPaymentMethodSelected);
+    $document.on("click", Constants.DELETE_CARD_BUTTON_ID, onCardDeletion);
+    $document.on("click", Constants.USE_CARD_BUTTON_ID, onCardSelected);
+    $document.on("submit", Constants.PAYMENT_FORM_ID, onPaymentFormSubmit);
 }
 
 /**
@@ -386,11 +390,11 @@ function initializeHandlers()
  */
 function initializeForm(tokenId = null)
 {
-    getCardList()
-    getFormData(tokenId)
+    getCardList();
+    getFormData(tokenId);
 }
 
 jQuery(function () {
-    setSpinnerState(SpinnerState.VISIBLE)
-    initializeHandlers()
+    setSpinnerState(SpinnerState.VISIBLE);
+    initializeHandlers();
 })
