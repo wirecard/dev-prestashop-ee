685fab9a59da1bce2427ff7f6b3250e819787af4
Richard Blechinger <me@blechi.at>
2019-10-31 14:02:31 +0100

    Add tests

---
+++ Test/Unit/Helper/TemplateHelperTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Shop System Extensions:
+ * - Terms of Use can be found at:
+ * https://github.com/wirecard/prestashop-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/prestashop-ee/blob/master/LICENSE
+ */
+
+use WirecardEE\Prestashop\Helper\TemplateHelper;
+
+class TemplateHelperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testItReturnsCorrectPath() {
+        $path = TemplateHelper::getTemplatePath('creditcard');
+
+        $this->assertEquals(
+            'modules/wirecardpaymentgateway/views/templates/front/creditcard.tpl',
+            $path
+        );
+    }
+}
