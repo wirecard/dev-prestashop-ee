f5d954742870c288895024d8c48f517280065aab
Flavius Aspra <flavius.aspra@wirecard.com>
2019-12-21 09:05:05 +0100

    Make static analysis happier

--- wirecardpaymentgateway/classes/Notification/Success.php
+++ wirecardpaymentgateway/classes/Notification/Success.php
@@ -76,10 +76,12 @@ abstract class Success implements ProcessablePaymentNotification
 
                 $parentTransaction = $this->getParentTransaction();
                 $parentTransaction->markSettledAsClosed();
-                $parentTransaction->updateOrder($this->order,
-                                                $this->notification,
-                                                $this->order_manager,
-                                                $this->order_service);
+                $parentTransaction->updateOrder(
+                    $this->order,
+                    $this->notification,
+                    $this->order_manager,
+                    $this->order_service
+                );
             }
         } catch (\Exception $e) {
             error_log("\t\t\t" . __METHOD__ . ' ' . __LINE__ . ' ' . "exception: " . $e->getMessage());
