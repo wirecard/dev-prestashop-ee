9247ffcac05358921d60b43a0f881c97a96c8702
Flavius Aspra <flavius.aspra@wirecard.com>
2019-12-22 13:04:46 +0100

    Remove unused: creating order payment

--- wirecardpaymentgateway/classes/Response/PostProcessing/Success.php
+++ wirecardpaymentgateway/classes/Response/PostProcessing/Success.php
@@ -48,12 +48,8 @@ class Success extends SuccessAbstract
     public function process()
     {
         parent::process();
-        $tx_id = \Tools::getValue('tx_id');
-        $transaction = new Transaction($tx_id);
+        $transaction = new Transaction(\Tools::getValue('tx_id'));
         $transaction->markSettledAsClosed();
-        //$service = new OrderService($this->order);//TODO: bug in presta?
-        //$service->createOrderPayment((string)$transaction->getAmount(),
-        //$transaction->getPaymentMethod(), $transaction->getTransactionId());
 
         $this->context_service->setConfirmations(
             $this->getTranslatedString('success_new_transaction')
