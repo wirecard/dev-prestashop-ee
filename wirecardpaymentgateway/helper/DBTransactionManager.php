d7b05ee414ed2e5c18615c0c1501aaab7518fbd5
Flavius Aspra <flavius.aspra@wirecard.com>
2019-12-22 08:35:40 +0100

    Attempt to reduce cyclomatic complexity

--- wirecardpaymentgateway/helper/DBTransactionManager.php
+++ wirecardpaymentgateway/helper/DBTransactionManager.php
@@ -74,7 +74,7 @@ class DBTransactionManager
         if (!trim($name)) {
             throw new \RuntimeException("Invalid name for lock: $name");
         }
-        $backoffFactors = $this->getReasonableBackoffFactorsForTimeout($timeout);
+        $backoffFactors = $this->getReasonableBackoffFactorsForTimeout2($timeout);
         $attempts = 0;
         $startTime = microtime(true);
         $sqlTimeout = $timeout;
@@ -135,47 +135,40 @@ class DBTransactionManager
      * 59s => 10 x 10ms + 9 x 100ms + 8 x 1s + 10 x 5s
      * 60s => 10 x 10ms + 9 x 100ms + 9 x 1s + 10 x 5s
      *
-     * @return array
+     * @return array intervals in millisecond
      */
-    private function getReasonableBackoffFactorsForTimeout($timeout)
+    private function getReasonableBackoffFactorsForTimeout2($timeoutInSeconds)
     {
-        $timeoutInMilliseconds = $timeout * 1000;
+        $timeoutInMilliseconds = $timeoutInSeconds * 1000;
         $coveredTime = 0;
         $intervals = [];
         $lastIntervalCount = 0;
+        $regularIntervalSpecs = [
+            ['upperBound' => 100, 'duration' => 10],//first 100 ms are divided into 10 x 10ms
+            ['upperBound' => 1000, 'duration' => 100],//interval between 100ms and 1s is divided into 9 x 100ms
+            ['upperBound' => 10 * 1000, 'duration' => 1000],//between 1s and 10s is divided into 9 x 1s
+            ['upperBound' => 60 * 1000, 'duration' => 5 * 1000],//between 10s and 60s is divided into 10 x 5s
+            ['upperBound' => 10 * 60 * 1000, 'duration' => 60 * 1000],//between 60s and 10m is divided into 9 x 1m
+            ['upperBound' => 60 * 60 * 1000, 'duration' => 5 * 60 * 1000],// 10m to 60m is divided into 10 x 5m
+        ];
+        $specIndex = 0;
         while ($coveredTime < $timeoutInMilliseconds) {//at most 58 loops
-            if ($coveredTime < 100) {//first 100 ms are divided into 10 x 10ms
-                $intervals[] = 10;//10 ms
-                $coveredTime += 10;
-                continue;
-            }
-            if ($coveredTime < 1000) {//interval between 100ms and 1s is divided into 9 x 100ms
-                $intervals[] = 100;//100 ms
-                $coveredTime += 100;
-                continue;
-            }
-            if ($coveredTime < 10 * 1000) {//interval between 1s and 10s
-                $intervals[] = 1000;//1s
-                $coveredTime += 1000;
-                continue;
-            }
-            if ($coveredTime < 60 * 1000) {//interval between 10s and 60s
-                $intervals[] = 5 * 1000;//5s
-                $coveredTime += 5 * 1000;
-                continue;
-            }
-            if ($coveredTime < 10 * 60 * 1000) {//interval between 60s and 10m
-                $intervals[] = 60 * 1000;//1m
-                $coveredTime += 60 * 1000;
-                continue;
-            }
-            if ($coveredTime < 60 * 60 * 1000) {//interval between 10m and 60m
-                $intervals[] = 5 * 60 * 1000;//5m
-                $coveredTime += 5 * 60 * 1000;
-                $lastIntervalCount++;
-                if ($lastIntervalCount < 10) {
-                    continue;
+            $upperTimeBound = $regularIntervalSpecs[$specIndex]['upperBound'];
+            $sleepDuration = $regularIntervalSpecs[$specIndex]['duration'];
+            if ($coveredTime < $upperTimeBound) {
+                $intervals[] = $sleepDuration;
+                $coveredTime += $sleepDuration;
+                if ($specIndex == count($regularIntervalSpecs) - 1) {
+                    $lastIntervalCount++;
+                    if($lastIntervalCount < 10) {
+                        continue;
+                    }
                 }
+                continue;
+            }
+            $specIndex++;
+            if (isset($regularIntervalSpecs[$specIndex])) {
+                continue;
             }
             if ($coveredTime < $timeoutInMilliseconds) {//add the final (default) interval
                 $intervals[] = 10 * 60 * 1000;//10m
