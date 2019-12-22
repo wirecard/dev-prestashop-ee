d53ddcd0716766f09ceef030f2b1d81b4fa4378f
Flavius Aspra <flavius.aspra@wirecard.com>
2019-12-22 09:48:08 +0100

    Fix order state for purchase

--- wirecardpaymentgateway/classes/Notification/Success.php
+++ wirecardpaymentgateway/classes/Notification/Success.php
@@ -98,10 +98,11 @@ abstract class Success implements ProcessablePaymentNotification
      */
     private function getParentTransaction()
     {
-        $transaction = Transaction::getInitialTransactionForOrder($this->order->reference);
-
-        if ($transaction) {
-            return $transaction;
+        if($this->notification->getTransactionType() != \Wirecard\PaymentSdk\Transaction\Transaction::TYPE_PURCHASE) {
+            $transaction = Transaction::getInitialTransactionForOrder($this->order->reference);
+            if ($transaction) {
+                return $transaction;
+            }
         }
 
         return new InitialTransaction($this->notification->getRequestedAmount()->getValue());
