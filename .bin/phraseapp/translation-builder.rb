3720a51a65b3e63eb9408f7c950a00c2bb7b1aa9
Sebastijan Sakac <sebastijan.sakac@wirecard.com>
2020-04-15 08:26:30 +0200

    Remove unused functions from transaction builder

--- .bin/phraseapp/translation-builder.rb
+++ .bin/phraseapp/translation-builder.rb
@@ -78,13 +78,7 @@ class TranslationBuilder
     translation_keys = file.read.scan(/->getTranslatedString\(\'(.*)\'\)/).uniq
 
     file.rewind
-    translation_keys += file.read.scan(/->getTranslatedString\(\'(.*)\'\, \$lang_code\)/).uniq
-
-    file.rewind
     translation_keys += file.read.scan(/->getTranslationForLanguage\(\$lang->iso\_code, \'(.*)\', \$this->name\)/).uniq
-
-    file.rewind
-    translation_keys += file.read.scan(/->getTranslationForLanguage\(\$lang, \'(.*)\', \$file\)/).uniq
 
     file.rewind
     translation_keys += file.read.scan(/new\sAdminControllerTabConfig\(\n.*\n.*\'(.*)\'/).uniq
